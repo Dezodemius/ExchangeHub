f57ac45938105f706d5617d7e3221efc8c907e56
Dezodemius <gladkovyegor@gmail.com>
2025-11-13 17:25:56 +0400

    Убрал лишнее. Вроде, сканер валют работает

--- ExchangeHub.CurrencyWorker/CurrencyDbContext.cs
+++ ExchangeHub.CurrencyWorker/CurrencyDbContext.cs
@@ -11,16 +11,4 @@ public class CurrencyDbContext : DbContext
         : base(options)
     {
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Currency>(entity =>
-        {
-            entity.ToTable("currency");
-            entity.HasKey(c => c.Id);
-            entity.Property(c => c.Code).IsRequired();
-            entity.Property(c => c.Name).IsRequired();
-            entity.Property(c => c.Rate).HasColumnType("numeric(18,4)");
-        });
-    }
 }
